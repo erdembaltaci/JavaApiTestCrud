0d2a54f3eac7421a843fc62c4d0ad2919353717d
Ali Erdem Baltacı <erdembaltaci5609@gmail.com>
2026-05-15 15:44:08 +0300

    feat: updated test cases and presentation

--- src/test/java/DeleteTests.java
+++ src/test/java/DeleteTests.java
@@ -6,7 +6,7 @@ public class DeleteTests {
     @Test
     public void test_DeleteAccount() {
         // Not: Burada daha önce oluşturulmuş veya mevcut bir ID'yi (örneğin 4) seçmelisin
-        String endpoint = "https://6a06eb14c83ba8ad9b3e2406.mockapi.io/accounts/accounts/2";
+        String endpoint = "https://6a06eb14c83ba8ad9b3e2406.mockapi.io/accounts/accounts/1";
 
         Response response = given().when().delete(endpoint);
 
--- src/test/java/UpdateTests.java
+++ src/test/java/UpdateTests.java
@@ -8,22 +8,27 @@ public class UpdateTests {
     @Test
     public void test_UpdateAccount() {
         String endpoint = "https://6a06eb14c83ba8ad9b3e2406.mockapi.io/accounts/accounts/1";
-        String updateBody = "{\"name\": \"Updated Ali Erdem\"}";
+
+        // Gönderilen veri
+        String nameValue = "Updated fvrf Ali Erdem";
+        String updateBody = "{\"name\": \"" + nameValue + "\"}";
 
         Response response = given()
                 .contentType(ContentType.JSON)
                 .body(updateBody)
                 .when()
-                .put(endpoint);
+                .patch(endpoint); // Teknik doğruluk için PATCH yaptık
 
-        // Kontroller
-        response.then().statusCode(200).body("name", equalTo("Updated Ali Erdem"));
+        // Kontroller - nameValue değişkenini kullanarak hata riskini sıfırladık
+        response.then()
+                .statusCode(200)
+                .body("name", equalTo(nameValue));
 
-        System.out.println("========== PUT TEST RAPORU ==========");
+        System.out.println("========== PATCH TEST RAPORU ==========");
         System.out.println("Durum Kodu (Status): " + response.getStatusCode());
         System.out.println("Yanıt Süresi: " + response.getTime() + " ms");
         System.out.println("Güncellenmiş Veri (JSON):");
         System.out.println(response.getBody().asPrettyString());
-        System.out.println("=====================================");
+        System.out.println("=======================================");
     }
 }
